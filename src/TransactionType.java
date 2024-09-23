4ae89ede6034a6a643ee8747bdae91ebb363c46e
erkeaiymaidosova <erkeaiym.aidosova@alatoo.edu.kg>
2024-09-23 15:37:46 +0600

    fifth commit

--- src/TransactionType.java
+++ src/TransactionType.java
@@ -1,7 +1,3 @@
-public class TransactionType {
-    public static TransactionType DEPOSIT;
-    public static TransactionType WITHDRAWAL;
-    public enum Transaction{
-        DEPOSIT,WITHDRAWAL
-    }
+public enum TransactionType {
+    DEPOSIT,WITHDRAWAL
 }
